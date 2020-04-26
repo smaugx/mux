649a5783e2b0e7ef48e580cd7d5c5b935e25171a
smaugx <linuxcode2niki@gmail.com>
2020-04-26 11:30:43 +0800

    add message_handler.h

--- message_handle/message_handler.h
+++ message_handle/message_handler.h
@@ -15,8 +15,10 @@ namespace mux {
 
 namespace transport {
 
+using OnDispatchCallback = std::function<void(transport::SocketDataPtr&)>;
+
 class ThreadConsuemr;
-class MultiMessageHandler;
+class MessageHandler;
 
 class ThreadConsumer {
 public:
@@ -26,13 +28,60 @@ public:
     ThreadConsumer& operator=(ThreadConsumer&& other)      = delete;
 
     ThreadConsumer(
-            std::shared_ptr<MultiMessageHandler> multi_handler,
+            std::shared_ptr<MessageHandler> message_handler,
             std::mutex& mutex,
             std::condition_variable& cond_var);
     virtual ~ThreadConsumer();
 
 public:
+    void Join();
+    void RegisterOnDispatchCallback(OnDispatchCallback callback);
+    void UnRegisterOnDispatchCallback();
 
+private:
+    void LoopHandleMessage();
+
+private:
+    std::shared_ptr<MessageHandler> message_handler_;
+    std::mutex& mutex_;
+    std::condition_variable& cond_var_;
+    bool destroy_ {false};
+    std::shared_ptr<std::thread> message_handle_thread_ {nullptr};
+    std::mutex callback_mutex_;
+    OnDispatchCallback callback_;
+};
+
+typedef std::shared_ptr<ThreadConsumer> ThreadConsumerPtr;
+
+
+class MessageHandler : public std::enable_shared_from_this<MessageHandler> {
+public:
+    MessageHandler(const MessageHandler&)                 = delete;
+    MessageHandler& operator=(const MessageHandler&)      = delete;
+    MessageHandler(MessageHandler&&)                      = delete;
+    MessageHandler& operator=(MessageHandler&&)           = delete;
+
+    MessageHandler();
+    ~MessageHandler();
+
+public:
+    bool Init();
+    void HandleMessage(SocketDataPtr& msg);
+    SocketDataPtr GetMessageFromQueue();
+    void RegisterOnDispatchCallback(OnDispatchCallback callback);
+    void UnRegisterOnDispatchCallback();
+
+private:
+    void Join();
+
+private:
+    std::mutex priority_queue_map_mutex_;
+    std::map<uint32_t, std::queue<SocketDataPtr>> priority_queue_map_;
+    std::mutex mutex_;
+    std::condition_variable cond_var_;
+    std::vector<ThreadConsumerPtr> consumer_vec_;
+    std::mutex inited_mutex_;
+    bool inited_ {false};
 };
 
 }  // namespace transport
